65484affe46ed436d600eb11d6f1eab354c00250
ijerkovic <igor.jerkovic@tolar.io>
2022-03-31 16:59:54 +0200

    Input form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { TextInput } from 'react-native';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      solSource: '',
+    };
+
+    this.contractUpdated = this.contractUpdated.bind(this)
+  }
+
+  handleClick() {
+    console.log("----INPUT----")
+    console.log(this.state.solSource);
+    console.log("----END----")
+  }
+
+  contractUpdated(contract) {
+    console.log(contract);
+    this.setState({
+      solSource: contract,
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <TextInput
+          style={{
+            height: 500,
+            width: '70%',
+            margin: 5,
+            padding: 5,
+            backgroundColor: "white"
+          }}
+          placeholder="Paste Solidity code to verify here..."
+          multiline={true}
+          onChangeText={this.contractUpdated}
+        />
+        <br/>
+            <button
+              style={{
+                height: 40,
+                margin: 5,
+              }}
+              onClick={() => this.handleClick()}> Verify
+            </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
